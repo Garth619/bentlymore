8e33a88ea675793ccb8a7b4a30dd7d34fc940674
Garrett Cullen <Garth619@users.noreply.github.com>
2017-10-02 14:15:22 -0700

    push

--- wp-content/themes/bentlymore/homepage_template_parts/section-1.php
+++ wp-content/themes/bentlymore/homepage_template_parts/section-1.php
@@ -1,5 +1,20 @@
 <section class="banner">
 	
+	<span class="banner_content">Telling Your Story</span>
 	
+	<a class="begin" href="">begin with a free consultation</a><!-- begin -->
+	
+	<div class="scroll_wrapper">
+		
+		<div class="scroll_lines">
+			
+			<span class="orange_line"></span><!-- orange_line -->
+			<span class="white_line"></span><!-- white_line -->
+			
+		</div><!-- scroll_lines -->
+		
+		<span class="scroll">Scroll</span><!-- scroll -->
+		
+	</div><!-- scroll_wrapper -->
 	
 </section><!-- banner -->
