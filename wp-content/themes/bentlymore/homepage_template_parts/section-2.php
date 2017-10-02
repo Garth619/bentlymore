29fb0cb6885fc29b078061db96149d53eeb7cf89
Garrett Cullen <Garth619@users.noreply.github.com>
2017-10-02 14:48:32 -0700

    push

---
+++ wp-content/themes/bentlymore/homepage_template_parts/section-2.php
@@ -0,0 +1,30 @@
+<section class="section_two">
+	
+	<div class="inner_section_two">
+	
+		<div class="settlements_wrapper">
+		
+			
+			<div class="settlement_slideshow_wrapper">
+				
+				<div class="settlement_slideshow">
+					
+					<div class="settlement_slides">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/result-ico-01.svg"/>
+						
+						<span class="sett_amount">$31.5m</span><!-- sett_amount -->
+						
+					</div><!-- settlement_slides -->
+					
+				</div><!-- settlement_slideshow -->
+				
+			</div><!-- settlement_slideshow_wrapper -->
+		
+		
+		</div><!-- settlements_wrapper -->
+	
+	</div><!-- inner_section_two -->
+	
+	
+</section><!-- section_two -->
