1bbd724dc65742f77761319233a940923f7bdc8b
Garrett Cullen <Garth619@users.noreply.github.com>
2017-10-04 15:46:35 -0700

    push

---
+++ wp-content/themes/bentlymore/homepage_template_parts/section-3.php
@@ -0,0 +1,41 @@
+<section class="section_three">
+	
+	<div class="inner_section_three">
+		
+		<div class="title_wrapper">
+			
+			<h1>Orange County</h1>
+			<h2>Personal Injury Attorneys</h2>
+			
+		</div><!-- title_wrapper -->
+		
+		<div class="section_three_content content">
+			
+			<div class="section_three_image_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/img-01.png"/>
+				
+			</div><!-- section_three_image_wrapper -->
+			
+			<p>Curabitur a mauris et nisi malesuada malesuada eu non nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id enim tempus, rutrum felis non, aliquam leo. Mauris volutpat a nulla accumsan ullamcorper. Aliquam erat tortor, congue in vestibulum sit amet, bibendum vitae lectus. Duis eleifend nisi fringilla sem pretium, non auctor metus egestas. Pellentesque vel mauris sit amet dui molestie faucibus in quis enim. Aliquam dictum at justo eu malesuada. Pellentesque ullamcorper tincidunt accumsan. Cras finibus tortor non lacus lacinia varius. Sed nec scelerisque nibh. Aenean feugiat congue quam, sed scelerisque ex luctus ut.</p>
+			
+			
+			<div class="blockquote_wrapper">
+				
+				<div class="image_wrapper">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/award-content.png"/>
+					
+				</div><!-- image_wrapper -->
+				
+				<span class="text_wrapper">Nunc bibendum molestie mauris, sed semper nisi sodales quis orci nunc vel efinent conditumet.</span><!-- text_wrapper -->
+				
+			</div><!-- blockquote_wrapper -->
+
+
+			
+		</div><!-- section_three_content -->
+		
+	</div><!-- inner_section_three -->
+	
+</section><!-- section_three -->
