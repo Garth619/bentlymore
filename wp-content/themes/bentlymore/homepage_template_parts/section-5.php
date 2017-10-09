eeff8b2d4b5b46712ddbd280184a749a281a0549
Garrett Cullen <Garth619@users.noreply.github.com>
2017-10-09 15:59:33 -0700

    push

---
+++ wp-content/themes/bentlymore/homepage_template_parts/section-5.php
@@ -0,0 +1,13 @@
+<section class="section_five">
+	
+	
+	<div class="section_five_inner">
+		
+		
+		
+		
+		
+	</div><!-- section_five_inner -->
+	
+	
+</section><!-- section_five -->
--- wp-content/themes/bentlymore/page-home.php
+++ wp-content/themes/bentlymore/page-home.php
@@ -5,11 +5,11 @@
 	get_header(); ?>
 
 
-
 <?php get_template_part( 'homepage_template_parts/section', '1' );?>
 <?php get_template_part( 'homepage_template_parts/section', '2' );?>
 <?php get_template_part( 'homepage_template_parts/section', '3' );?>
 <?php get_template_part( 'homepage_template_parts/section', '4' );?>
+<?php get_template_part( 'homepage_template_parts/section', '5' );?>
 
 
 <?php get_footer(); ?>
