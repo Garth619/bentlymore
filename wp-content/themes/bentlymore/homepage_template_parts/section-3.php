1d756197656fe8203404f47d3cc65b5e8c1c413e
Garrett Cullen <Garth619@users.noreply.github.com>
2017-10-09 11:15:43 -0700

    push

--- wp-content/themes/bentlymore/homepage_template_parts/section-3.php
+++ wp-content/themes/bentlymore/homepage_template_parts/section-3.php
@@ -17,20 +17,31 @@
 				
 			</div><!-- section_three_image_wrapper -->
 			
-			<p>Curabitur a mauris et nisi malesuada malesuada eu non nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id enim tempus, rutrum felis non, aliquam leo. Mauris volutpat a nulla accumsan ullamcorper. Aliquam erat tortor, congue in vestibulum sit amet, bibendum vitae lectus. Duis eleifend nisi fringilla sem pretium, non auctor metus egestas. Pellentesque vel mauris sit amet dui molestie faucibus in quis enim. Aliquam dictum at justo eu malesuada. Pellentesque ullamcorper tincidunt accumsan. Cras finibus tortor non lacus lacinia varius. Sed nec scelerisque nibh. Aenean feugiat congue quam, sed scelerisque ex luctus ut.</p>
+			<div class="desktop_content_wrap">
+				
+				<p>Curabitur a mauris et nisi malesuada malesuada eu non nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id enim tempus, rutrum felis non, aliquam leo. Mauris volutpat a nulla accumsan ullamcorper. Aliquam erat tortor, congue in vestibulum sit amet, bibendum vitae lectus. Duis eleifend nisi fringilla sem pretium, non auctor metus egestas. Pellentesque vel mauris sit amet dui molestie faucibus in quis enim. Aliquam dictum at justo eu malesuada. Pellentesque ullamcorper tincidunt accumsan. Cras finibus tortor non lacus lacinia varius. Sed nec scelerisque nibh. Aenean feugiat congue quam, sed scelerisque ex luctus ut.</p>
 			
 			
-			<div class="blockquote_wrapper">
+				<div class="blockquote_wrapper">
 				
-				<div class="image_wrapper">
+					<div class="image_wrapper">
 					
-					<img src="<?php bloginfo('template_directory');?>/images/award-content.png"/>
+						<img src="<?php bloginfo('template_directory');?>/images/award-content.png"/>
 					
-				</div><!-- image_wrapper -->
+					</div><!-- image_wrapper -->
 				
-				<span class="text_wrapper">Nunc bibendum molestie mauris, sed semper nisi sodales quis orci nunc vel efinent conditumet.</span><!-- text_wrapper -->
+					<span class="text_wrapper">Nunc bibendum molestie mauris, sed semper nisi sodales quis orci nunc vel efinent conditumet.</span><!-- text_wrapper -->
 				
-			</div><!-- blockquote_wrapper -->
+				</div><!-- blockquote_wrapper -->
+			
+				<p>Morbi imperdiet orci ut viverra lacinia. Fusce fringilla vitae risus vestibulum volutpat. Curabitur semper scelerisque dui in interdum. Suspendisse eleifend finibus viverra. Quisque imperdiet, odio vitae condimentum volutpat, neque nisl suscipit risus, nec fringilla mauris tortor sit amet justo. Donec bibendum iaculis magna, eu auctor nisi placerat ut. Quisque cursus velit a gravida tempor. Vestibulum vel eleifend dolor. In laoreet ultricies tellus id mollis. Nullam in dui tincidunt, dignissim lorem ut, accumsan magna.</p> 
+
+				<h2>Nunc bibendum molestie mauris, sed semper nisi sodales quis orci nunc conditumet.</h2>
+
+				<p>Curabitur a mauris et nisi malesuada malesuada eu non nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id enim tempus, rutrum felis non, aliquam leo. Mauris volutpat a nulla accumsan ullamcorper. Aliquam erat tortor, congue in vestibulum sit amet, bibendum vitae lectus. Duis eleifend nisi fringilla sem pretium, non auctor metus egestas. Pellentesque vel mauris sit amet dui molestie faucibus in quis enim. Aliquam dictum at justo eu malesuada. Pellentesque ullamcorper tincidunt accumsan. Cras finibus tortor non lacus lacinia varius. Sed nec scelerisque nibh. Aenean feugiat congue quam, sed scelerisque ex luctus ut. Nunc bibendum molestie mauris, sed semper nisi sodales quis. Morbi imperdiet orci ut viverra lacinia. Fusce fringilla vitae risus vestibulum volutpat. Curabitur semper scelerisque dui in interdum. Suspendisse eleifend finibus viverra. Quisque imperdiet, odio vitae condimentum volutpat, neque nisl suscipit risus, nec fringilla mauris tortor sit amet justo. Donec bibendum iaculis magna, eu auctor nisi placerat ut. Quisque cursus velit a gravida tempor. Vestibulum vel eleifend dolor. In laoreet ultricies tellus id mollis. Nullam in dui tincidunt, dignissim lorem ut, accumsan magna.</p>
+
+
+		</div><!-- desktop_content_wrap -->
 
 
 			
