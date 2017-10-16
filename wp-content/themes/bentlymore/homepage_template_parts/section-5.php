519f8c6d0a0cb3130396cc571b8eabe2920eb121
Garrett Cullen <Garth619@users.noreply.github.com>
2017-10-16 16:15:51 -0700

    push

--- wp-content/themes/bentlymore/homepage_template_parts/section-5.php
+++ wp-content/themes/bentlymore/homepage_template_parts/section-5.php
@@ -51,7 +51,7 @@
 
 <p>Curabitur a mauris et nisi malesuada malesuada eu non nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id enim tempus, rutrum felis non, aliquam leo. Mauris volutpat a nulla accumsan ullamcorper. Aliquam erat tortor, congue in vestibulum sit amet, bibendum vitae lectus.</p>
 
-<p>Nunc bibendum molestie mauris, sed semper nisi sodales quis. Morbi imperdiet orci ut viverra lacinia. Fusce fringilla vitae risus vestibulum volutpat. Curabitur semper scelerisque dui in interdum. Suspendisse eleifend finibus viverra. Quisque imperdiet, odio vitae condimentum volutpat, neque nisl suscipit risus, nec fringilla mauris tortor sit amet justo. Donec bibendum iaculis magna, eu auctor nisi placerat ut. Quisque cursus velit a gravida tempor. Vestibulum vel eleifend dolor. In laoreet ultricies tellus id mollis. Nullam in dui tincidunt, dignissim lorem ut, accumsan magna.</p>
+<p>Nunc bibendum molestie mauris, sed semper nisi sodales quis. Morbi imperdiet orci ut viverra lacinia. Fusce fringilla vitae risus vestibulum volutpat. Curabitur semper scelerisque dui in interdum. Suspendisse eleifend finibus viverra. Quisque imperdiet, odio vitae condimentum volutpat, neque nisl suscipit risus, nec fringilla mauris tortor sit amet justo. Donec bibendum iaculis magna, eu auctor nisi placerat ut. Quisque cursus velit a gravida tempor. Vestibulum vel eleifend dolor. In laoreet ultricies tellus id mollis.</p>
 			
 			
 			
@@ -63,11 +63,10 @@
 			
 			<p> Duis eleifend nisi fringilla sem pretium, non auctor metus egestas. Pellentesque vel mauris sit amet dui molestie faucibus in quis enim. Aliquam dictum at justo eu malesuada. Pellentesque ullamcorper tincidunt accumsan. Cras finibus tortor non lacus lacinia varius. Sed nec scelerisque nibh. Aenean feugiat congue quam, sed scelerisque ex luctus ut.</p>
 			
-			<p>Morbi imperdiet orci ut viverra lacinia. Fusce fringilla vitae risus vestibulum volutpat. Curabitur semper scelerisque dui in interdum. Suspendisse eleifend finibus viverra. Quisque imperdiet, odio vitae condimentum volutpat, neque nisl suscipit risus, nec fringilla mauris tortor sit amet justo. Donec bibendum iaculis magna, eu auctor nisi placerat ut. Quisque cursus velit a gravida tempor. Vestibulum vel eleifend dolor. In laoreet ultricies tellus id mollis. Nullam in dui tincidunt, dignissim lorem ut, accumsan magna. </p>
+			<blockquote>Nunc bibendum molestie nunc mauris, sed semper nisi sodales interdum quis.</blockquote>
+			
+			<p>Morbi imperdiet orci ut viverra lacinia. Fusce fringilla vitae risus vestibulum volutpat. Curabitur semper scelerisque dui in interdum. Suspendisse eleifend finibus viverra. Quisque imperdiet, odio vitae condimentum volutpat, neque nisl suscipit risus, nec fringilla mauris tortor sit amet justo. Donec bibendum iaculis magna, eu auctor nisi placerat ut. Quisque cursus velit a gravida tempor. Vestibulum vel eleifend dolor. In laoreet ultricies tellus id mollis. </p>
 
-<p>Curabitur a mauris et nisi malesuada malesuada eu non nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id enim tempus, rutrum felis non, aliquam leo. Mauris volutpat a nulla accumsan ullamcorper. Aliquam erat tortor, congue in vestibulum sit amet, bibendum vitae lectus. Duis eleifend nisi fringilla sem pretium, non auctor metus egestas. Pellentesque vel mauris sit amet dui molestie faucibus in quis enim. Aliquam dictum at justo eu malesuada. Pellentesque ullamcorper tincidunt accumsan. Cras finibus tortor non lacus lacinia varius. Sed nec scelerisque nibh. Aenean feugiat congue quam, sed scelerisque ex luctus ut.</p>
-
-<p>Nunc bibendum molestie mauris, sed semper nisi sodales quis. Morbi imperdiet orci ut viverra lacinia. Fusce fringilla vitae risus vestibulum volutpat. Curabitur semper scelerisque dui in interdum. Suspendisse eleifend finibus viverra. Quisque imperdiet, odio vitae condimentum volutpat, neque nisl suscipit risus, nec fringilla mauris tortor sit amet justo. Donec bibendum iaculis magna, eu auctor nisi placerat ut. Quisque cursus velit a gravida tempor. Vestibulum vel eleifend dolor. In laoreet ultricies tellus id mollis. Nullam in dui tincidunt, dignissim lorem ut, accumsan magna.</p>			
 			
 			
 		</div><!-- col -->
--- wp-content/themes/bentlymore/page-home.php
+++ wp-content/themes/bentlymore/page-home.php
@@ -2,14 +2,13 @@
 	
 	/* Template Name: Home */
 	
-	get_header(); ?>
+ get_header();
 
+ get_template_part( 'homepage_template_parts/section', '1' );
+ get_template_part( 'homepage_template_parts/section', '2' );
+ get_template_part( 'homepage_template_parts/section', '3' );
+ get_template_part( 'homepage_template_parts/section', '4' );
+ get_template_part( 'homepage_template_parts/section', '5' );
+ get_template_part( 'homepage_template_parts/section', '6' );
 
-<?php get_template_part( 'homepage_template_parts/section', '1' );?>
-<?php get_template_part( 'homepage_template_parts/section', '2' );?>
-<?php get_template_part( 'homepage_template_parts/section', '3' );?>
-<?php get_template_part( 'homepage_template_parts/section', '4' );?>
-<?php get_template_part( 'homepage_template_parts/section', '5' );?>
-
-
-<?php get_footer(); ?>
+ get_footer(); ?>
