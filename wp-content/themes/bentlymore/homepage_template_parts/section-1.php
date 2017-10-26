26ce1c3f33bf2b2f3d4df8bfc6d456ae8fd81800
Garrett Cullen <Garth619@users.noreply.github.com>
2017-10-26 14:49:49 -0700

    push

--- wp-content/themes/bentlymore/homepage_template_parts/section-1.php
+++ wp-content/themes/bentlymore/homepage_template_parts/section-1.php
@@ -1,8 +1,10 @@
 <section class="banner">
 	
-	<span class="banner_content">Telling Your Story</span>
+	<span class="banner_content">Telling<br/> Your Story</span>
 	
-	<a class="begin" href="">begin with a free consultation</a><!-- begin -->
+	<a class="begin desktop" href="">begin with a free consultation</a><!-- begin -->
+	
+	<a class="begin mobile" href="">free consultation</a><!-- begin -->
 	
 	<div class="scroll_wrapper">
 		
