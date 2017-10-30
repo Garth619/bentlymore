593d7add81aeada01cf0a55ba90d6da361ae3ec7
Garrett Cullen <Garth619@users.noreply.github.com>
2017-10-30 15:34:24 -0700

    push

---
+++ wp-content/themes/bentlymore/page-practiceareas.php
@@ -0,0 +1,87 @@
+<?php
+	
+/* Template Name: Practice Areas Directory */	
+	
+get_header(); ?>
+
+<div class="main">
+	
+	<div class="inner_main">
+		
+		
+		
+		<div class="one_column">
+			
+			<div class="internal_header_wrapper content">
+				
+				<h1><?php the_title();?></h1>
+				
+				<span class="internal_gold_line"></span><!-- internal_line -->
+				
+				</div><!-- internal_header_wrapper -->
+				
+				<div class="pa_wrapper">
+					
+					
+					<div class="inner_pa_border_wrapper">
+						
+						
+						<ul>
+							
+							
+							<li><a href="">Rollover</a></li>
+							<li><a href="">Practice Areas  This is a Practice Areas</a></li>
+							<li><a href="">Practice Areas  This is a Practice Areas</a></li>
+							<li><a href="">Practice Areas  This is a Practice Areas</a></li>
+							<li><a href="">This is a Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							
+							<li><a href="">Practice Areas Test</a></li>
+							<li><a href="">Practice Areas Test</a></li>
+							<li><a href="">Practice Areas Test</a></li>
+							<li><a href="">Practice Areas Test</a></li>
+							<li><a href="">Practice Areas Test</a></li>
+							<li><a href="">Practice Areas Test</a></li>
+							<li><a href="">Practice Areas Test</a></li>
+							
+							
+							
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							
+							<li><a href="">Practice Areas</a></li>
+							
+							
+						</ul>
+						
+						
+					</div><!-- inner_pa_border_wrapper -->
+					
+					<div class="inner_pa_wrapper">
+						
+						
+					</div><!-- inner_pa_wrapper -->
+					
+					
+				</div><!-- pa_wrapper -->
+				
+		
+			
+			
+		</div><!-- content -->
+		
+		
+	</div><!-- inner_main -->
+	
+	
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
