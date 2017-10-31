cca681ac498a5718bb82611819a2627d760efdb5
Garrett Cullen <Garth619@users.noreply.github.com>
2017-10-31 13:40:15 -0700

    push

---
+++ wp-content/themes/bentlymore/page-attorneysdirectory.php
@@ -0,0 +1,184 @@
+<?php
+	
+/* Template Name: Attorneys Directory */	
+	
+get_header(); ?>
+
+<div class="main">
+	
+	<div class="inner_main">
+		
+		
+		
+		<div class="one_column">
+			
+			<div class="internal_header_wrapper content">
+				
+				<h1><?php the_title();?></h1>
+				
+				<span class="internal_gold_line"></span><!-- internal_line -->
+				
+				</div><!-- internal_header_wrapper -->
+				
+				<div class="internal_attorneys_wrapper">
+					
+					
+					<div class="single_attorney_wrapper">
+						
+						<a class="" href="">
+						
+						<div class="inner_attorney_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/profile-greg.jpg"/>
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+						
+						</div><!-- inner_attorney_wrapper -->
+						
+						<span class="large_header">Greg Bently</span><!-- large_header -->
+						
+						<span class="sub_header">Partner</span>
+						
+						</a>
+						
+					</div><!-- single_attorney_wrapper -->
+					
+					
+					<div class="single_attorney_wrapper">
+						
+						<a class="" href="">
+						
+						<div class="inner_attorney_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/profile-greg.jpg"/>
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+						
+						</div><!-- inner_attorney_wrapper -->
+						
+						<span class="large_header">Greg Bently</span><!-- large_header -->
+						
+						<span class="sub_header">Partner</span>
+						
+						</a>
+						
+					</div><!-- single_attorney_wrapper -->
+					
+					
+					<div class="single_attorney_wrapper">
+						
+						<a class="" href="">
+						
+						<div class="inner_attorney_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/profile-greg.jpg"/>
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+						
+						</div><!-- inner_attorney_wrapper -->
+						
+						<span class="large_header">Greg Bently</span><!-- large_header -->
+						
+						<span class="sub_header">Partner</span>
+						
+						</a>
+						
+					</div><!-- single_attorney_wrapper -->
+					
+					
+					<div class="single_attorney_wrapper">
+						
+						<a class="" href="">
+						
+						<div class="inner_attorney_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/profile-greg.jpg"/>
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+						
+						</div><!-- inner_attorney_wrapper -->
+						
+						<span class="large_header">Greg Bently</span><!-- large_header -->
+						
+						<span class="sub_header">Partner</span>
+						
+						</a>
+						
+					</div><!-- single_attorney_wrapper -->
+					
+					<div class="single_attorney_wrapper">
+						
+						<a class="" href="">
+						
+						<div class="inner_attorney_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/profile-greg.jpg"/>
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+						
+						</div><!-- inner_attorney_wrapper -->
+						
+						<span class="large_header">Greg Bently</span><!-- large_header -->
+						
+						<span class="sub_header">Partner</span>
+						
+						</a>
+						
+					</div><!-- single_attorney_wrapper -->
+					
+					<div class="single_attorney_wrapper">
+						
+						<a class="" href="">
+						
+						<div class="inner_attorney_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/profile-greg.jpg"/>
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+						
+						</div><!-- inner_attorney_wrapper -->
+						
+						<span class="large_header">Greg Bently</span><!-- large_header -->
+						
+						<span class="sub_header">Partner</span>
+						
+						</a>
+						
+					</div><!-- single_attorney_wrapper -->
+					
+					<div class="single_attorney_wrapper">
+						
+						<a class="" href="">
+						
+						<div class="inner_attorney_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/profile-greg.jpg"/>
+							
+							<span class="view_profile">View Profile</span><!-- view_profile -->
+						
+						</div><!-- inner_attorney_wrapper -->
+						
+						<span class="large_header">Greg Bently</span><!-- large_header -->
+						
+						<span class="sub_header">Partner</span>
+						
+						</a>
+						
+					</div><!-- single_attorney_wrapper -->
+					
+					
+				</div><!-- attorneys_wrapper -->		
+		
+			
+			
+		</div><!-- content -->
+		
+		
+	</div><!-- inner_main -->
+	
+	
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
