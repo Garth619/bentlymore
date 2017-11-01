e25d087c64a28a730e817ec4e4fb7a17a9a8e838
Garrett Cullen <Garth619@users.noreply.github.com>
2017-11-01 10:57:40 -0700

    push

--- wp-content/themes/bentlymore/page-contact.php
+++ wp-content/themes/bentlymore/page-contact.php
@@ -52,9 +52,25 @@ get_header(); ?>
 				<div class="contact_wrapper">
 					
 					
-					<div class="inner_contact_address content"></div><!-- inner_contact_address -->
+					<div class="inner_contact_address content">
+						
+						<a class="contact_link" href="https://www.google.com/maps/place/4+Park+Plaza+%23500,+Irvine,+CA+92614/@33.6782353,-117.840085,17z/data=!3m1!4b1!4m5!3m4!1s0x80dcde86c6583ebf:0x3c96a2b5dc2b12ed!8m2!3d33.6782353!4d-117.8378963" target="_blank">4 Park Plaza #500<br/> Irvine, CA 92614</a>
+						
+						<span class="contact_line"></span><!-- contact_line -->
+						
+						<a class="contact_directions" href="https://www.google.com/maps/place/4+Park+Plaza+%23500,+Irvine,+CA+92614/@33.6782353,-117.840085,17z/data=!3m1!4b1!4m5!3m4!1s0x80dcde86c6583ebf:0x3c96a2b5dc2b12ed!8m2!3d33.6782353!4d-117.8378963">Directions</a><!-- contact_directions -->
+						
+						<a class="contact_tel" href="tel:(949) 870-3800">(949) 870-3800</a>
+						
+					</div><!-- inner_contact_address -->
 					
-					<div class="contact_map"></div><!-- contact_map -->
+					<div class="contact_map">
+						
+						
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3320.1864788564108!2d-117.84008498479471!3d33.67823528070933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcde86c6583ebf%3A0x3c96a2b5dc2b12ed!2s4+Park+Plaza+%23500%2C+Irvine%2C+CA+92614!5e0!3m2!1sen!2sus!4v1509556687154" frameborder="0" style="border:0" allowfullscreen></iframe>
+						
+						
+					</div><!-- contact_map -->
 					
 					
 				</div><!-- contact_wrapper -->
