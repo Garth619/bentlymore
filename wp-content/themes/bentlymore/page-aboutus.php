25de37b92512fae2779e6655bcfd3f9ccff2edd1
Garrett Cullen <Garth619@users.noreply.github.com>
2017-11-01 14:02:20 -0700

    push

---
+++ wp-content/themes/bentlymore/page-aboutus.php
@@ -0,0 +1,73 @@
+<?php
+	
+/* Template Name: About Us */	
+	
+get_header(); ?>
+
+
+
+
+<div class="main internal_banner_main">
+	
+	<section class="banner">
+	
+	<span class="banner_content">Telling<br/> Your Story</span>
+	
+	<a class="begin desktop" href="">begin with a free consultation</a><!-- begin -->
+	
+	<a class="begin mobile" href="">free consultation</a><!-- begin -->
+	
+	<div class="scroll_wrapper">
+		
+		<div class="scroll_lines">
+			
+			<span class="orange_line"></span><!-- orange_line -->
+			<span class="white_line"></span><!-- white_line -->
+			
+		</div><!-- scroll_lines -->
+		
+		<span class="scroll">Scroll</span><!-- scroll -->
+		
+	</div><!-- scroll_wrapper -->
+	
+</section><!-- banner -->
+
+	
+	<div class="inner_main">
+		
+		
+		
+		<div class="one_column">
+			
+			
+			<div class="internal_header_wrapper content">
+				
+				<h1><?php the_title();?></h1>
+				
+				<span class="internal_gold_line"></span><!-- internal_line -->
+				
+				</div><!-- internal_header_wrapper -->
+				
+				
+				<div class="about_wrapper">
+					
+					
+					
+					
+				</div><!-- about_wrapper -->
+					
+					
+				</div><!-- testimonials_wrapper -->
+			
+			
+		</div><!-- one_column -->
+		
+		
+	</div><!-- inner_main -->
+	
+	
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/bentlymore/page-videocenter.php
@@ -0,0 +1,331 @@
+<?php
+	
+/* Template Name: Video Center */	
+	
+get_header(); ?>
+
+<div class="main">
+	
+	<div class="inner_main">
+		
+		
+		
+		<div class="one_column">
+			
+			<div class="internal_header_wrapper content">
+				
+				<h1><?php the_title();?></h1>
+				
+				<span class="internal_gold_line"></span><!-- internal_line -->
+				
+				</div><!-- internal_header_wrapper -->
+				
+				<div class="video_center_wrapper">
+					
+					<div class="single_video_wrapper">
+						
+						<div class="inner_video_wrapper">
+							
+							<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+							
+							<div class="play_wrapper">
+								
+								
+								<svg class="vc_play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 99 99">
+									
+									<defs>
+									
+									
+									
+									</defs><title>play</title>
+									
+									
+									<circle id="circle_two" class="vc_cls-1" cx="49.5" cy="49.5" r="49.5"/>
+									
+									<path id="rec_two" data-name="Rectangle 18 copy 2" class="vc_cls-2" d="M44.8,36.3l20,15-20,15"/>
+									<path class="vc_cls-2" d="M-66.5-67"/>
+									
+								</svg>
+								
+								
+							</div><!-- play_wrapper -->
+							
+							
+						</div><!-- inner_video_wrapper -->
+						
+						
+						
+						<span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverContent=link">
+						
+							<a href="#">
+						
+								<span class="video_center_title">Video Title Goes here</span><!-- video_title -->
+								<span class="video_center_play_title">Play</span><!-- video_center_play_title -->
+						
+							</a>
+						
+						</span>
+						
+						
+					</div><!-- single_video_wrapper -->
+					
+					
+					<div class="single_video_wrapper">
+						
+						<div class="inner_video_wrapper">
+							
+							<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+							
+							<div class="play_wrapper">
+								
+								
+								<svg class="vc_play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 99 99">
+									
+									<defs>
+									
+									
+									
+									</defs><title>play</title>
+									
+									
+									<circle id="circle_two" class="vc_cls-1" cx="49.5" cy="49.5" r="49.5"/>
+									
+									<path id="rec_two" data-name="Rectangle 18 copy 2" class="vc_cls-2" d="M44.8,36.3l20,15-20,15"/>
+									<path class="vc_cls-2" d="M-66.5-67"/>
+									
+								</svg>
+								
+								
+							</div><!-- play_wrapper -->
+							
+							
+						</div><!-- inner_video_wrapper -->
+						
+						
+						
+						<span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverContent=link">
+						
+							<a href="#">
+						
+								<span class="video_center_title">Video Title Goes here</span><!-- video_title -->
+								<span class="video_center_play_title">Play</span><!-- video_center_play_title -->
+						
+							</a>
+						
+						</span>
+						
+						
+					</div><!-- single_video_wrapper -->
+					
+					
+					<div class="single_video_wrapper">
+						
+						<div class="inner_video_wrapper">
+							
+							<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+							
+							<div class="play_wrapper">
+								
+								
+								<svg class="vc_play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 99 99">
+									
+									<defs>
+									
+									
+									
+									</defs><title>play</title>
+									
+									
+									<circle id="circle_two" class="vc_cls-1" cx="49.5" cy="49.5" r="49.5"/>
+									
+									<path id="rec_two" data-name="Rectangle 18 copy 2" class="vc_cls-2" d="M44.8,36.3l20,15-20,15"/>
+									<path class="vc_cls-2" d="M-66.5-67"/>
+									
+								</svg>
+								
+								
+							</div><!-- play_wrapper -->
+							
+							
+						</div><!-- inner_video_wrapper -->
+						
+						
+						
+						<span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverContent=link">
+						
+							<a href="#">
+						
+								<span class="video_center_title">Video Title Goes here</span><!-- video_title -->
+								<span class="video_center_play_title">Play</span><!-- video_center_play_title -->
+						
+							</a>
+						
+						</span>
+						
+						
+					</div><!-- single_video_wrapper -->
+					
+					
+					<div class="single_video_wrapper">
+						
+						<div class="inner_video_wrapper">
+							
+							<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+							
+							<div class="play_wrapper">
+								
+								
+								<svg class="vc_play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 99 99">
+									
+									<defs>
+									
+									
+									
+									</defs><title>play</title>
+									
+									
+									<circle id="circle_two" class="vc_cls-1" cx="49.5" cy="49.5" r="49.5"/>
+									
+									<path id="rec_two" data-name="Rectangle 18 copy 2" class="vc_cls-2" d="M44.8,36.3l20,15-20,15"/>
+									<path class="vc_cls-2" d="M-66.5-67"/>
+									
+								</svg>
+								
+								
+							</div><!-- play_wrapper -->
+							
+							
+						</div><!-- inner_video_wrapper -->
+						
+						
+						
+						<span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverContent=link">
+						
+							<a href="#">
+						
+								<span class="video_center_title">Video Title Goes here</span><!-- video_title -->
+								<span class="video_center_play_title">Play</span><!-- video_center_play_title -->
+						
+							</a>
+						
+						</span>
+						
+						
+					</div><!-- single_video_wrapper -->
+					
+					
+					<div class="single_video_wrapper">
+						
+						<div class="inner_video_wrapper">
+							
+							<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+							
+							<div class="play_wrapper">
+								
+								
+								<svg class="vc_play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 99 99">
+									
+									<defs>
+									
+									
+									
+									</defs><title>play</title>
+									
+									
+									<circle id="circle_two" class="vc_cls-1" cx="49.5" cy="49.5" r="49.5"/>
+									
+									<path id="rec_two" data-name="Rectangle 18 copy 2" class="vc_cls-2" d="M44.8,36.3l20,15-20,15"/>
+									<path class="vc_cls-2" d="M-66.5-67"/>
+									
+								</svg>
+								
+								
+							</div><!-- play_wrapper -->
+							
+							
+						</div><!-- inner_video_wrapper -->
+						
+						
+						
+						<span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverContent=link">
+						
+							<a href="#">
+						
+								<span class="video_center_title">Video Title Goes here</span><!-- video_title -->
+								<span class="video_center_play_title">Play</span><!-- video_center_play_title -->
+						
+							</a>
+						
+						</span>
+						
+						
+					</div><!-- single_video_wrapper -->
+					
+					
+					<div class="single_video_wrapper">
+						
+						<div class="inner_video_wrapper">
+							
+							<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+							
+							<div class="play_wrapper">
+								
+								
+								<svg class="vc_play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 99 99">
+									
+									<defs>
+									
+									
+									
+									</defs><title>play</title>
+									
+									
+									<circle id="circle_two" class="vc_cls-1" cx="49.5" cy="49.5" r="49.5"/>
+									
+									<path id="rec_two" data-name="Rectangle 18 copy 2" class="vc_cls-2" d="M44.8,36.3l20,15-20,15"/>
+									<path class="vc_cls-2" d="M-66.5-67"/>
+									
+								</svg>
+								
+								
+							</div><!-- play_wrapper -->
+							
+							
+						</div><!-- inner_video_wrapper -->
+						
+						
+						
+						<span class="wistia_embed wistia_async_dzuvhsasnw popover=true popoverContent=link">
+						
+							<a href="#">
+						
+								<span class="video_center_title">Video Title Goes here</span><!-- video_title -->
+								<span class="video_center_play_title">Play</span><!-- video_center_play_title -->
+						
+							</a>
+						
+						</span>
+						
+						
+					</div><!-- single_video_wrapper -->
+					
+					
+					<script src="https://fast.wistia.com/embed/medias/dzuvhsasnw.jsonp" async></script>
+					<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+					
+				</div><!-- video_center_wrapper -->
+				
+		
+			
+			
+		</div><!-- content -->
+		
+		
+	</div><!-- inner_main -->
+	
+	
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
