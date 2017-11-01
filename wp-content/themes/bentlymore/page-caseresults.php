25d053c675fe80891af54ae3fa7a4a2b9d6d1136
Garrett Cullen <Garth619@users.noreply.github.com>
2017-11-01 15:07:45 -0700

    push

---
+++ wp-content/themes/bentlymore/page-caseresults.php
@@ -0,0 +1,212 @@
+<?php
+	
+/* Template Name: Case Results */	
+	
+get_header(); ?>
+
+<div class="main">
+	
+	<div class="inner_main">
+		
+		
+		
+		<div class="one_column">
+			
+			<div class="internal_header_wrapper content">
+				
+				<h1><?php the_title();?></h1>
+				
+				<span class="internal_gold_line"></span><!-- internal_line -->
+				
+				</div><!-- internal_header_wrapper -->
+				
+				
+				<div class="int_case_results_wrapper">
+					
+					
+					<div class="int_single_case_results">
+						
+						<div class="int_single_case_border">
+							
+							<div class="int_case_content">
+							
+								<span class="int_case_amount">$31.5m</span><!-- int_case_amount -->
+							
+								<span class="int_case_line"></span><!-- int_case_line -->
+							
+								<span class="int_case_type">Dangerous Roads</span><!-- int_case_type -->
+							
+								<span class="int_result">Verdict</span><!-- int_result -->
+							
+							</div><!-- int_case_content -->
+							
+						</div><!-- int_single_case_border -->
+						
+						<div class="int_case_bg_wrapper">
+							
+							<div class="int_case_bg_wrapper_inner">
+								
+								
+								
+							</div><!-- int_case_bg_wrapper_inner -->
+							
+							<div class="int_case_hover">
+									
+									<div class="int_case_hover_inner content">
+										
+										<p>Curabitur a mauris et nisi malesuada malesuada eu non nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id enim tempus, rutrum felis non, aliquam leo.</p>
+										
+									</div><!-- int_case_hover_inner -->
+									
+							</div><!-- int_case_hover -->
+							
+						</div><!-- int_case_bg_wrapper -->
+						
+					</div><!-- int_single_case_results -->
+					
+					
+					
+					<div class="int_single_case_results">
+						
+						<div class="int_single_case_border">
+							
+							<div class="int_case_content">
+							
+								<span class="int_case_amount">$31.5m</span><!-- int_case_amount -->
+							
+								<span class="int_case_line"></span><!-- int_case_line -->
+							
+								<span class="int_case_type">Dangerous Roads</span><!-- int_case_type -->
+							
+								<span class="int_result">Verdict</span><!-- int_result -->
+							
+							</div><!-- int_case_content -->
+							
+						</div><!-- int_single_case_border -->
+						
+						<div class="int_case_bg_wrapper">
+							
+							<div class="int_case_bg_wrapper_inner">
+								
+								
+								
+							</div><!-- int_case_bg_wrapper_inner -->
+							
+							<div class="int_case_hover">
+									
+									<div class="int_case_hover_inner content">
+										
+										<p>Curabitur a mauris et nisi malesuada malesuada eu non nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id enim tempus, rutrum felis non, aliquam leo.</p>
+										
+									</div><!-- int_case_hover_inner -->
+									
+							</div><!-- int_case_hover -->
+							
+						</div><!-- int_case_bg_wrapper -->
+						
+					</div><!-- int_single_case_results -->
+					
+					
+					
+					
+					<div class="int_single_case_results">
+						
+						<div class="int_single_case_border">
+							
+							<div class="int_case_content">
+							
+								<span class="int_case_amount">$31.5m</span><!-- int_case_amount -->
+							
+								<span class="int_case_line"></span><!-- int_case_line -->
+							
+								<span class="int_case_type">Dangerous Roads</span><!-- int_case_type -->
+							
+								<span class="int_result">Verdict</span><!-- int_result -->
+							
+							</div><!-- int_case_content -->
+							
+						</div><!-- int_single_case_border -->
+						
+						<div class="int_case_bg_wrapper">
+							
+							<div class="int_case_bg_wrapper_inner">
+								
+								
+								
+							</div><!-- int_case_bg_wrapper_inner -->
+							
+							<div class="int_case_hover">
+									
+									<div class="int_case_hover_inner content">
+										
+										<p>Curabitur a mauris et nisi malesuada malesuada eu non nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id enim tempus, rutrum felis non, aliquam leo.</p>
+										
+									</div><!-- int_case_hover_inner -->
+									
+							</div><!-- int_case_hover -->
+							
+						</div><!-- int_case_bg_wrapper -->
+						
+					</div><!-- int_single_case_results -->
+					
+					
+					
+					
+					<div class="int_single_case_results">
+						
+						<div class="int_single_case_border">
+							
+							<div class="int_case_content">
+							
+								<span class="int_case_amount">$31.5m</span><!-- int_case_amount -->
+							
+								<span class="int_case_line"></span><!-- int_case_line -->
+							
+								<span class="int_case_type">Dangerous Roads</span><!-- int_case_type -->
+							
+								<span class="int_result">Verdict</span><!-- int_result -->
+							
+							</div><!-- int_case_content -->
+							
+						</div><!-- int_single_case_border -->
+						
+						<div class="int_case_bg_wrapper">
+							
+							<div class="int_case_bg_wrapper_inner">
+								
+								
+								
+							</div><!-- int_case_bg_wrapper_inner -->
+							
+							<div class="int_case_hover">
+									
+									<div class="int_case_hover_inner content">
+										
+										<p>Curabitur a mauris et nisi malesuada malesuada eu non nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id enim tempus, rutrum felis non, aliquam leo.</p>
+										
+									</div><!-- int_case_hover_inner -->
+									
+							</div><!-- int_case_hover -->
+							
+						</div><!-- int_case_bg_wrapper -->
+						
+					</div><!-- int_single_case_results -->
+					
+					
+					
+					
+				</div><!-- int_case_results_wrapper -->
+		
+			
+			
+		</div><!-- one_column -->
+		
+		
+	</div><!-- inner_main -->
+	
+	
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
